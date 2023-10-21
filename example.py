46a1e3a0dbce1ea553922676740c855ce571de21
Dev Patel <devpatel2556@gmail.com>
2023-10-21 16:20:06 -0400

    class recurssion implemeneted

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
 class Main(): 
     name = "Test"
     class Main2():
-        
-        class Main3():
+        class Main4():
             pass
-    
-    def __init__(self, name: int) -> None:
+        class Main5():
+            pass
+    class Main3():
         pass
+    
+    # def __init__(self, name: int) -> None:
+    #     pass
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from _ast import AST
 import ast
+from typing import Any
 
 module = "example.py"
 with open(module, "r") as file:
@@ -8,39 +10,40 @@ with open(module, "r") as file:
 class Visitor(ast.NodeVisitor):
     def __init__(self, module):
         self.module = module.split(".")[0]
-        self.parent = None
+        self.current = {}
         self.data = {}
-
-    def generic_visit(self, node):
-        #print(type(node).__name__, self.data)
-        if isinstance(node, ast.Module) or isinstance(node, ast.ClassDef) or isinstance(node, ast.FunctionDef):
-            previous_parent = self.parent
-            self.parent = self.module if isinstance(node, ast.Module) else node.name
-            ast.NodeVisitor.generic_visit(self, node)   
-            self.parent = previous_parent
-        else:
-            ast.NodeVisitor.generic_visit(self, node)
     
     def visit_Module(self, node):
         self.data[self.module] = {}
+        self.current = self.data[self.module]
         self.generic_visit(node)
 
     def visit_ClassDef(self, node):
-        print(node.name, self.parent)
+        prev = self.current
+        prev[node.name] = {}
+        self.current = prev[node.name]
+
+        data = {
+            "type": "class"
+        }
+
         self.generic_visit(node)
-    
+        prev[node.name].update(data)
+        self.current = prev
+        
     def visit_FunctionDef(self, node):
-        print(node.name, self.parent)
-        self.generic_visit(node)
+        self.generic_visit(node)  
 
 
 
 
 visitor = Visitor(module)
 visitor.visit(tree)
+with open("data.json", "w") as file:
+    import json
+    json.dump(visitor.data, file, indent=4)
 
 
-print(visitor.data)
 
 
 
